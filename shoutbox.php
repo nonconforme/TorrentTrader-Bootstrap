8b13473bc182281a5219b711237f557bfbf0aac6
Patience <ayrtonalbuaguiar@gmail.com>
2015-07-19 14:15:51 -0300

    Update Shoutbox.php

--- shoutbox.php
+++ shoutbox.php
@@ -56,11 +56,11 @@ if ($CURUSER){
 		$THEME = $ss_a["uri"];
 }
 
-if(!isset($_GET['history'])){ ?>
-
+if(!isset($_GET['history'])){ 
+?>
 <html>
 <head>
-	<title><?php echo $site_config['SITENAME'].':'.T_("SHOUTBOX"); ?></title>
+	<title><?php echo $site_config['SITENAME'] . T_("SHOUTBOX"); ?></title>
 
 	<meta http-equiv="refresh" content="300" />
 	<!-- Bootstrap CSS -->
@@ -73,38 +73,35 @@ if(!isset($_GET['history'])){ ?>
   	<link rel="stylesheet" href="themes/<?php echo $THEME; ?>/css/font-awesome.min.css">
 	<script src="backend/java_klappe.js"></script>
 </head>
-<body>
-
-	<?php }else{
+<?php
+}else{
     
     if ($site_config["MEMBERSONLY"]) {
         loggedinonly();
     }
     
 	stdhead();
-	begin_frame(T_("SHOUTBOX_HISTORY"));
+	begin_frame(T_("SHOUTBOX_HISTORY")); ?>
 
-	$query = 'SELECT COUNT(*) FROM shoutbox';
-	$result = SQL_Query_exec($query);
-	$row = mysql_fetch_row($result); ?>
 	<nav class="text-center">
-  		<ul class="pagination">
-	<?php $pages = round($row[0] / 100) + 1;
+		<ul class="pagination">
+
+	<?php $query = 'SELECT COUNT(*) FROM shoutbox';
+	$result = SQL_Query_exec($query);
+	$row = mysql_fetch_row($result);
+	$pages = round($row[0] / 100) + 1;
 	$i = 1;
 	while ($pages > 0){ ?>
 
 			<li><a href='shoutbox.php?history=1&amp;page=<?php echo $i;?>'><?php echo $i;?></a></li>
 
-	<?php $i++;
+		<?php $i++;
 		$pages--;
 	} ?>
 
 		</ul>
 	</nav>
-	<dl class="dl-horizontal">
-	<?php }
-
-
+<?php }
 
 if (isset($_GET['history'])) {
 	if (isset($_GET['page'])) {
@@ -123,32 +120,69 @@ if (isset($_GET['history'])) {
 }else{
 	$query = 'SELECT * FROM shoutbox ORDER BY msgid DESC LIMIT 20';
 }
+?>
+<dl class="dl-horizontal <?php echo (isset($_GET['history']) ? '' : 'shoutbox' ); ?>">
+<?php
 
 $result = SQL_Query_exec($query);
 $alt = false;
 
 while ($row = mysql_fetch_assoc($result)) {
 	if ($alt){	
+		echo '';
 		$alt = false;
 	}else{
+		echo '';
 		$alt = true;
 	} ?>
 
-	<dt><?php echo date('jS M, g:ia', utc_to_tz_time($row['date'])); ?></dt>
 
-<?php if ( ($CURUSER["edit_users"]=="yes") || ($CURUSER['username'] == $row['user']) ){ ?>
-		
-	<dd><a href='shoutbox.php?del=<?php echo $row['msgid']; ?>'><i class="fa fa-trash-o"></i></a>
+	<dt><?php echo date('jS M, g:ia', utc_to_tz_time($row['date']));?></dt>
 	
+
+	<?php if ( ($CURUSER["edit_users"]=="yes") || ($CURUSER['username'] == $row['user']) ){ ?>
+		<dd><a href="shoutbox.php?del=<?php echo $row['msgid'];?>"><i class="fa fa-trash-o"></i></a>
 	<?php } ?>
 
-	<a href="account-details.php?id=<?php echo $row['userid']; ?>" target="_parent"><strong><?php echo $row['user']; ?>:</strong></a><?php echo nl2br(format_comment($row['message'])); ?></dd>
-
-	<?php } ?>
+	<a href="account-details.php?id=<?php echo $row['userid'];?>" target="_parent"><strong><?php echo $row['user'];?>:</strong></a><?php echo nl2br(format_comment($row['message']));?></dd>
+<?php } ?>
 
 </dl>
 
-<?php 
+<?php
+
+//if the user is logged in, show the shoutbox, if not, dont.
+if(!isset($_GET['history'])) {
+	if (isset($_COOKIE["pass"])){ ?>
+		<form name='shoutboxform' action='index.php' method='post'>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="row">
+						<div class="col-sm-8">
+							<div class="input-group">
+								<input type='text' name='message' class='form-control' />
+								<span class="input-group-btn">
+									<button type='submit' name='submit' class="btn btn-success"/><?php echo T_("SHOUT"); ?></button>
+								</span>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="btn-group" role="group" aria-label="...">
+        						<a href="javascript:PopMoreSmiles('shoutboxform', 'message');" class="btn btn-default"><?php echo T_("MORE_SMILIES"); ?></a>
+        						<a href="javascript:PopMoreTags();" class="btn btn-default"><?php echo T_("TAGS"); ?></a>
+								<a href='shoutbox.php' class="btn btn-default"><?php echo T_("REFRESH"); ?></a>              
+								<a href='shoutbox.php?history=1' target='_blank' class="btn btn-default"><?php echo T_("HISTORY"); ?></a>
+							</div>
+						</div>
+					</div><br />
+					<p class="text-center"><?php printf(T_("SHOUTBOX_REFRESH"), 5);?></p>
+				</div>
+			</div>
+		</form>
+<?php	}else{
+		echo "<br /><div class='shoutbox_error'>".T_("SHOUTBOX_MUST_LOGIN")."</div>";
+	}
+}
 
 if(!isset($_GET['history'])){ 
 	echo "</body></html>";
@@ -163,4 +197,3 @@ else{
 	echo T_("SHOUTBOX_DISABLED");
 }
 ?>
-
